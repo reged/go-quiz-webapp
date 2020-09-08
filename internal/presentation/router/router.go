9d92d9ca30df555d325058abec8e99c11d4eb4d5
Pavel Ivannikov <ivannikovpv@gmail.com>
2020-09-08 18:55:04 +0300

    Fix path to static folder

--- internal/presentation/router/router.go
+++ internal/presentation/router/router.go
@@ -1,9 +1,10 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/hackersandslackers/golang-helloworld/internal/presentation/controller"
-	"net/http"
 )
 
 // Route declaration
@@ -16,7 +17,7 @@ func InitRouter(c *controller.Controller) *mux.Router {
 	r.HandleFunc("/login", c.LoginHandler).Methods("POST")
 	r.HandleFunc("/rebuild", c.RebuildHandler).Methods("GET")
 	r.HandleFunc("/test", c.TestHandler).Methods("GET")
-	fileServer := http.FileServer(http.Dir("./static"))
+	fileServer := http.FileServer(http.Dir("../../static"))
 	r.PathPrefix("/").Handler(http.StripPrefix("/static", fileServer))
 	return r
 }
